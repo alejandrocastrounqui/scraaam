05d2bc6a1e9a453d1fdcdd2d2f596efda66fbfab
Alejandro Castro <alejandrocastrounqui@gmail.com>
2017-07-11 02:16:23 -0300

    e2e wait ec elementToBeClickable

--- test/e2e/project.create.test.js
+++ test/e2e/project.create.test.js
@@ -9,14 +9,22 @@ describe("project creation", () => {
 
   it("Should increment project amount when create new", async() => {
 
+    const ec = protractor.ExpectedConditions;
+
     browser.get(appUrl)
-    await element(by.css(".projects-menu .dropdown-toggle")).click()
+    const beforeDropDown  = element(by.css(".projects-menu .dropdown-toggle"))
+    browser.wait(ec.elementToBeClickable(beforeDropDown), 5000);
+    await beforeDropDown.click()
     const beforeCreate = await element.all(by.css(".projects-menu .project-item")).count()
     await element(by.css(".projects-menu .create-project")).click()
     await element(by.css("local-project-create-modal input[name=name]")).sendKeys("nuevo proyecto " + new Date())
     await element(by.css("local-project-create-modal .modal-create")).click()
 
     browser.get(appUrl)
+    const afterDropDown  = element(by.css(".projects-menu .dropdown-toggle"))
+    browser.wait(ec.elementToBeClickable(afterDropDown), 5000);
+    await afterDropDown.click()
+    
     await element(by.css(".projects-menu .dropdown-toggle")).click()
     const afterCreate = await element.all(by.css(".projects-menu .project-item")).count()
     afterCreate.should.be.equal(beforeCreate + 1)
